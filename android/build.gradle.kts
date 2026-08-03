13e4595d1f6bbeaf5e2a63ca0b526644ee1c5114
fgce2605 <fgce2605@gmail.com>
2026-08-03 18:14:28 +0530

    Refactor build.gradle.kts for plugin management
    Replaced allprojects configuration with pluginManagement and dependencyResolutionManagement sections.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,9 +1,20 @@
-allprojects {
-    configurations.all {
-        resolutionStrategy {
-            force("org.jetbrains.kotlin:kotlin-stdlib:1.8.22")
-        }
-        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk7")
-        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
     }
 }
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "android"
+include(":app")
+
+apply(from = "capacitor.settings.gradle")
