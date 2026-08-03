bd3bd5ef98d87438a8ee4adffb70185ad207adb0
fgce2605 <fgce2605@gmail.com>
2026-08-03 18:17:59 +0530

    Add resolution strategy for Kotlin dependencies

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -18,3 +18,12 @@ rootProject.name = "android"
 include(":app")
 
 apply(from = "capacitor.settings.gradle")
+allprojects {
+    configurations.all {
+        resolutionStrategy {
+            force("org.jetbrains.kotlin:kotlin-stdlib:1.8.22")
+        }
+        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk7")
+        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
+    }
+}
