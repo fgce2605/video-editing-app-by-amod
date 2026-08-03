b03752732ce632b3afa4ee374e9929cf191f447a
fgce2605 <fgce2605@gmail.com>
2026-08-03 18:28:19 +0530

    Delete android directory

--- android/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-allprojects {
-    configurations.all {
-        resolutionStrategy {
-            force("org.jetbrains.kotlin:kotlin-stdlib:1.8.22")
-        }
-        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk7")
-        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
-    }
-}
